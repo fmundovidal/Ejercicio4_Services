5b1ca276be64692faa0e4e77fe67a8d1bc131d52
ferran <fmundovidal@gmail.com>
2016-11-23 17:33:38 +0100

    Better performance

---
+++ app/src/main/java/com/example/a5alumno/ejercicio4_services/MyReceiver.java
@@ -0,0 +1,20 @@
+package com.example.a5alumno.ejercicio4_services;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+    private static final String TAG_BATTERY = "Low Battery";
+    public MyReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        Log.e("Mensaje","AIRPLANE MODE");
+    }
+}
